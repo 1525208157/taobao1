12032a5d7b040087b7bb1029bbf40da4981d46d9
1525208157 <1525208157@qq.com>
2018-03-21 10:02:41 +0800

    方法

--- src/main/java/org/taobao/pojo/Carts.java
+++ src/main/java/org/taobao/pojo/Carts.java
@@ -13,17 +13,11 @@ import javax.persistence.OneToOne;
 @Entity
 public class Carts { //购物车表
 	private Integer cartId; //购物车ID
-<<<<<<< HEAD
-	
-=======
+
  
->>>>>>> branch 'master' of https://github.com/1525208157/taobao1.git
 	private Users user; //用户
-<<<<<<< HEAD
 	private List<CartGoods> cartGoods; //一对多 购物车商品
-=======
 	private List<Specs> specs; //一对多 购物车商品
->>>>>>> branch 'master' of https://github.com/1525208157/taobao1.git
  
 	
 	@Id
