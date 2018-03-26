31f578712f94e7df1892317da1fc70b8611603bb
yangxinru <2278452749@qq.com>
2018-03-26 15:51:23 +0800

    yang

--- src/main/java/org/taobao/web/GoodsController.java
+++ src/main/java/org/taobao/web/GoodsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.taobao.pojo.Goods;
@@ -16,14 +17,15 @@ public class GoodsController {
 private GoodsService gs;
 
 @RequestMapping("/queryAll")
-@ResponseBody
-public List<Goods> queryAll(){
+
+public String queryAll(ModelMap map){
 	String sql="select * from goods";
 	List<Goods> gl=gs.queryAll(sql);
-	return gl;
+	map.put("gl", gl);
+	return "Goods";
 }
 @RequestMapping("/queryForBrandId")
-@ResponseBody
+
 public String queryForBrandId(Integer brandId){
 	String sql="select * from goods where1=1";
 	if (brandId!=null&&!brandId.equals("")) {
@@ -33,19 +35,22 @@ public String queryForBrandId(Integer brandId){
 	return "Goods";
 }
 @RequestMapping("/querySaleNum")
-@ResponseBody
+
 public String querySaleNum(){
-	String sql="select * from goods order by saleNum desc;";
+	String sql="select * from goods  order by saleNum desc;";
 	
 	List<Goods> gl=gs.queryAll(sql);
 	return "Goods";
 }
-@RequestMapping("/query")
-@ResponseBody
-public String query(){
-	String sql="select * from goods order by saleNum desc;";
-	
+@RequestMapping("/queryForName")
+
+public String query(String goodName){
+	String sql="select * from goods where 1=1";
+	if (goodName!=null&&!goodName.equals("")) {
+		sql=sql+" and goodName like '%"+goodName+"%'"	;
+	}
 	List<Goods> gl=gs.queryAll(sql);
 	return "Goods";
+
 }
 }
--- src/main/java/org/taobao/web/ShopsController.java
+++ src/main/java/org/taobao/web/ShopsController.java
@@ -1,8 +1,24 @@
 package org.taobao.web;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.taobao.pojo.Shops;
+import org.taobao.service.ShopsService;
 
 @Controller
 public class ShopsController {
-
+@Resource
+private ShopsService ss;
+public List<Shops> queryAll(String shopName){
+	String sql="select * from shops where 1=1;";
+	if (shopName!=null&&!shopName.equals("")) {
+		sql=sql+" and shopName like '%"+shopName+"%'";
+	}
+	sql=sql+" order by ";
+	List<Shops> sl=ss.queryAll(sql);
+	return sl;
+}
 }
