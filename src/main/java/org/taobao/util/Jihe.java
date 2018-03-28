a3ca6ff4779808c2c250eb3b1146096b298ebf0f
wj6611032 <1066862433@qq.com>
2018-03-28 10:25:08 +0800

    wj33

---
+++ src/main/java/org/taobao/util/Jihe.java
@@ -0,0 +1,17 @@
+package org.taobao.util;
+
+import java.util.List;
+
+public class Jihe {
+	private List<Integer> cartGoodIds;
+
+	public List<Integer> getCartGoodIds() {
+		return cartGoodIds;
+	}
+
+	public void setCartGoodIds(List<Integer> cartGoodIds) {
+		this.cartGoodIds = cartGoodIds;
+	}
+	
+
+}
