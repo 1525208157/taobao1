61a72b0e657d03a1edf660de40c529ad140ae507
wj6611032 <1066862433@qq.com>
2018-04-08 10:55:01 +0800

    wj323

--- src/main/java/org/taobao/web/UserController.java
+++ src/main/java/org/taobao/web/UserController.java
@@ -2,15 +2,21 @@ package org.taobao.web;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
+import org.apache.catalina.User;
+import org.hibernate.sql.Select;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.taobao.pojo.Users;
 import org.taobao.service.UserService;
+import org.taobao.util.Sign;
 
 @Controller
 @RequestMapping("/user")
@@ -28,26 +34,36 @@ public class UserController {
 	 * @throws IOException
 	 * 上传图片加回显图片
 	 */
-	@RequestMapping("/upload")
-	@ResponseBody
-	public String upload(MultipartFile img,HttpSession session) throws IllegalStateException, IOException {
+	@RequestMapping("/insert_user")
+	@ResponseBody//MultipartFile mfile
+	public String upload(MultipartFile img, Users user,HttpServletRequest request) throws  IOException {
+		System.out.println("到这里来了");
        //获得文件名加后缀名
-		String filename = img.getOriginalFilename();  
+		String filename = img.getOriginalFilename(); 
+		String userImg="/images/touxiang.png";
+		if(!filename.equals("")&&filename!=null){
 		//获得文件后缀名的出现位置前一个
         int spot = filename.lastIndexOf(".");
         //获得后缀名
         String ext = filename.substring(spot);
-        //获得文件上传的父路径
-        String path = session.getServletContext().getRealPath("images");
+        HttpSession session=request.getSession();
+	//获得文件上传的父路径
+       String path = session.getServletContext().getRealPath("images");
+       System.out.println("path:"+path);
         //获得当前时间的毫秒值和文件后缀名拼接成新的文件名
 		filename=System.currentTimeMillis()+ext;
 		//文件的保存
 		File file = new File(path,filename);
 		img.transferTo(file);
 		//文件的地址回复给浏览器让其通过此地址请求服务器获得刚上传的图片显示出来
-		String userImg="/images/"+filename;
+		 userImg="/images/"+filename;
+	    
 		//将文件的请求地址返回
-        return userImg;
+		}
+		user.setUserImg(userImg);
+	    ur.saveOrUpdate(user);
+	    
+        return "{}";
 		
 	}
 
@@ -60,5 +76,20 @@ public class UserController {
 	public String register(Users user) {
 		ur.saveOrUpdate(user);
 		return "ok";
+	}
+	
+	@RequestMapping("check_account")
+	@ResponseBody
+	public  Sign checkzhanghao(String account){
+		String sql="select * from users where account='"+account+"'";
+		List<Users> user=ur.selectUser(sql);
+		Sign s=new Sign();
+		s.setBiaoji("error");
+		if(user.isEmpty()){
+			s.setBiaoji("ok");
+		}
+		
+		return s;
+		
 	}
 }
